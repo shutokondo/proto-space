652a010b503e3e632e90e58ee1254f82dc56d72c
根東秀都 <shuto8201@gmail.com>
2015-10-02 02:13:29 +0900

    set logout

--- proto-space/app/controllers/application_controller.rb
+++ proto-space/app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameters_sanitizer.for(:sign_up).push(:name, :profile, :member, :works, :avatar)
+    devise_parameters_sanitizer.for(:account_update).push(:name, :profile, :member, :works, :avatar)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
