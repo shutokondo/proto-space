72c943912a323e49eb94259e09387ca3189099e7
根東秀都 <shuto8201@gmail.com>
2015-10-03 20:56:58 +0900

    edit posting prototype function

--- proto-space/app/controllers/prototypes_controller.rb
+++ proto-space/app/controllers/prototypes_controller.rb
@@ -1,5 +1,55 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def index
+    @prototypes = Prototype.order('created_at desc')
+  end
+
+  def show
+  end
+
+  def new
+    @prototype = Prototype.new
+    @prototype.prototype_images.build
+  end
+
+  def create
+    prototype = current_user.prototypes.new(proto_params)
+    if prototype.save
+      redirect_to action: :index, notice: "You have successfully created prototype."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @prototype_image = @prototype.prototype_images.build
+  end
+
+  def update
+    if @prototype.update(update_params)
+      @prototype.prototype_images
+      redirect_to action: :index, success: "You have successfully updated prototype."
+    else
+      render :edit, warning: "You have unfortunately failed to create."
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to :root, success: "You have successfully destroyed prototype."
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def proto_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept, prototype_images_attributes: [:name, :property])
+  end
+
+  def update_params
+    params.require(:prototype).permit(:id, :title, :catchcopy, :concept, prototype_images_attributes: [:name, :property])
   end
 end
--- proto-space/app/models/prototype.rb
+++ proto-space/app/models/prototype.rb
@@ -1,3 +1,18 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many :prototype_images, dependent: :delete_all
+
+  accepts_nested_attributes_for :prototype_images, reject_if: :reject_image
+
+  def main_image
+    prototype_images.main.first!
+  end
+
+  def sub_images
+    prototype_images.sub
+  end
+
+  def reject_image(attributed)
+    attributed["name"].blank?
+  end
 end
