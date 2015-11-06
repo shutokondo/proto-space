7cb8adfa933608e4b7c690340d188c3504cef627
根東秀都 <shuto8201@gmail.com>
2015-11-06 19:13:38 +0900

    edit users_controller

--- proto-space/app/controllers/users_controller.rb
+++ proto-space/app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    redirect_to root_path, danger: "Access denied", unless @user.id == current_user.id
+    redirect_to root_path, danger: "Access denied" unless @user.id == current_user.id
   end
 
   def update
-    if current_user.update(udpate_params)
+    if @user.update(udpate_params)
       redirect_to user_path(@user), success: "sccessfully updated your profile."
     else
       redirect_to edit_user_path(@user), warning: "unfortunately failed."
